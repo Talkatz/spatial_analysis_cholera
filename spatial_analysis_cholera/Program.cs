e278615534a1256ab2de1293f3643ab2eb54f743
Tal Katz <tal.cuthalion@gmail.com>
2020-05-06 12:58:01 +0300

    Add project files.

---
+++ spatial_analysis_cholera/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.IO;
+using System.Data.OleDb;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace spatial_analysis_cholera
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+			// importing the data from excel into dataTables
+			string pathPumps = @"C:\talkatz_github\arcobjects distance\project_upload\pumpsCsv.csv";
+			string pathAddresses = @"C:\talkatz_github\arcobjects distance\project_upload\addressesCsv.csv";
+            DataTable pumpsDt = ConvertCsvToDataTable(pathPumps, 3, ",");
+			pumpsDt.TableName = "pumpsDt";
+		    DataTable addressesDt = ConvertCsvToDataTable(pathAddresses, 6, ",");
+			pumpsDt.TableName = "addressesDt";
+
+
+			// creating a list of points from the coordinates
+			List<Point> pumpsPointsList = new List<Point>();
+			for (int i = 0; i < pumpsDt.Rows.Count; i++)
+			{
+				Point pnt = new Point(double.Parse(pumpsDt.Rows[i]["xPump"].ToString()), double.Parse(pumpsDt.Rows[i]["yPump"].ToString()));
+				pumpsPointsList.Add(pnt);
+			}
+			List<Point> choleraAddressesPointsList = new List<Point>();
+			for (int i = 0; i < addressesDt.Rows.Count; i++)
+			{
+				double residents = double.Parse(addressesDt.Rows[i]["buildingPop"].ToString());
+				double numOfCholeraCases = double.Parse(addressesDt.Rows[i]["choleraCases"].ToString());
+				if ((numOfCholeraCases / residents) >= 0.05) //if at least 5% of the residents in the building were diagnosed with cholera
+				{
+					Point pnt = new Point(double.Parse(addressesDt.Rows[i]["x"].ToString()), double.Parse(addressesDt.Rows[i]["y"].ToString()));
+					choleraAddressesPointsList.Add(pnt);
+				}
+			}
+
+
+			// Now we'll check which pump is the nearest to the addresses of the main cholera cases
+			pumpsDt.Columns.Add("avgDistToCholeraCases", typeof(double));			
+			for (int i = 0; i < pumpsDt.Rows.Count; i++)
+			{
+				double x = double.Parse(pumpsDt.Rows[i]["xPump"].ToString()); //pumpsDt.Rows[i]["avgDistToCholeraCases"].ToString());
+				double y = double.Parse(pumpsDt.Rows[i]["yPump"].ToString());
+				pumpsDt.Rows[i]["avgDistToCholeraCases"] = Point.distanceFromPump(x, y, choleraAddressesPointsList);
+				string pumpRow = String.Format("Pump number {0} average distance to the cholera cases is {1} meter.",
+					pumpsDt.Rows[i]["pumpId"].ToString(), String.Format("{0:0}", pumpsDt.Rows[i]["avgDistToCholeraCases"]));
+				Console.WriteLine(pumpRow);
+			}
+			// creating a new sorted table, in which the pumps on top will be those the smallest distance average, and that are needed accordingly to be checked first
+			DataView dv = pumpsDt.DefaultView;
+			dv.Sort = "avgDistToCholeraCases ASC";
+			DataTable sortedPumpsDt = dv.ToTable();
+
+
+			// using an implementation I did for the 'Nearest Neighbour Index' to see if the cholera cases are clustered or dispersed
+			double rectangleAreaOfCholeraCases = Point.rectangleArea(choleraAddressesPointsList);
+			double nearestNeighborIndex = Point.nearestNeighbourIndex(choleraAddressesPointsList, rectangleAreaOfCholeraCases);
+			if (nearestNeighborIndex < 1)
+			{
+				Console.WriteLine("The points pattern is clustered");
+			}
+			else if (nearestNeighborIndex >= 1)
+			{
+				Console.WriteLine("The points pattern is randomly dispersed");
+			}
+			else if ((nearestNeighborIndex > 1) && (nearestNeighborIndex < 2.15))
+			{
+				Console.WriteLine("The points pattern is regularly dispersed");
+			}
+			else if (nearestNeighborIndex >= 2.15)
+			{
+				Console.WriteLine("The points pattern is regularly uniform dispersed");
+			}
+		}
+
+		// a method to import an csv into a dataTable
+		public static DataTable ConvertCsvToDataTable(string csvFilePath, int numOfColumns, string delimiter)
+        {
+			DataTable dt = new DataTable();
+
+			using (var reader = new StreamReader(csvFilePath))
+			{
+				int count = 0;
+				while (!reader.EndOfStream)
+				{
+					var line = reader.ReadLine();
+					var values = line.Split(delimiter);
+					
+					if (count == 0) //header
+					{
+						for (int i =0; i < numOfColumns; i++)
+						{
+							string colName = values[i];
+							dt.Columns.Add(colName, typeof(String));
+						}
+					}
+					else //values
+					{
+						DataRow rowDt = dt.NewRow();
+						for (int i = 0; i < numOfColumns; i++)
+						{
+							rowDt[i] = values[i];
+						}
+						dt.Rows.Add(rowDt);
+					}
+					count++;
+				}
+			}
+			return dt;
+        }
+    }
+}
---
+++ spatial_analysis_cholera/points.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace spatial_analysis_cholera
+{
+    // a class I made for handling coordinates 
+    public class Point
+    {
+        public double x;
+        public double y;
+        public Point(double xCord, double yCord)
+        {
+            x = xCord;
+            y = yCord;
+        }
+
+        // a method the returns the distance between two points
+        public static double returnPointsDistance (Point one, Point two)
+        {
+            double dist = Math.Sqrt(Math.Pow(one.x - two.x, 2) + Math.Pow(one.y - two.y, 2));
+            return dist;
+        }
+
+        // a method that makes a rectangle that holds all the points and returns its area
+        public static double rectangleArea (List<Point> pntList)
+        {
+            double lowest_x = pntList.Min(point => point.x);
+            double lowest_y = pntList.Min(point => point.y);
+            double highest_x = pntList.Max(point => point.x);
+            double highest_y = pntList.Max(point => point.y);
+
+            Point north_west = new Point(lowest_x, highest_y);
+            Point north_east = new Point(highest_x, highest_y);
+            Point south_west = new Point(lowest_x, lowest_y);
+            Point south_east = new Point(highest_x, lowest_y);
+
+            double areaSqrm = (highest_y - lowest_y) * (highest_x - lowest_x);
+            return areaSqrm;
+        }
+
+        // a method that returns the nearest neighbour index, that tells if a set of points is clustered or dispersed randomly \ regularly
+        public static double nearestNeighbourIndex(List<Point> addressesPntList, double areaSqrm)
+        {
+            List<double> minDistListPoints = new List<double>();
+            List<double> minDistPerPoint = new List<double>();
+            for (int i = 0; i < addressesPntList.Count; i++)
+            {
+                double dist = 0;
+                for (int j = 0; j < addressesPntList.Count; j++)
+                {
+                    if (addressesPntList[i] != addressesPntList[j])
+                    {
+                        dist = Point.returnPointsDistance(addressesPntList[i], addressesPntList[j]);
+                        minDistPerPoint.Add(dist);
+                    }
+                }
+                minDistListPoints.Add(minDistPerPoint.Min());
+                minDistPerPoint.Clear();
+            }
+
+            double avgLowestDistace = minDistListPoints.Average();
+            double density = areaSqrm / minDistListPoints.Count;
+            double nearestNeighborIndex = avgLowestDistace / (0.5 * Math.Sqrt(density));
+            return nearestNeighborIndex;
+        }
+
+        // a method that returns the average distance from a point to a list of points
+        public static double distanceFromPump(double input_xCord, double input_yCord, List<Point> addressesPntList)
+        {
+            List<double> distList = new List<double>();
+            double average_distance = (-1);
+            Point pnt = new Point(input_xCord, input_yCord);
+            try
+            {
+                for (int i = 0; i < addressesPntList.Count; i++)
+                {
+                    double distToPump = 0;
+                    try
+                    {
+                        distToPump = Point.returnPointsDistance(addressesPntList[i], pnt);
+                        distList.Add(distToPump);
+                    }
+                    catch (Exception ex)
+                    {
+                        // do something with the error
+                        continue;
+                    }
+                }
+                average_distance = distList.Average();
+            }
+            catch (Exception ex)
+            {
+                // do something with the error
+                return average_distance;
+            }
+            return average_distance;
+        }
+
+
+    }
+}
